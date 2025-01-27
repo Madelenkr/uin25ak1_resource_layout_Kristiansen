26863731e138ea5dbd29fc44966828c8eab0469d
Madelenkr <142605372+Madelenkr@users.noreply.github.com>
2025-01-27 20:21:06 +0100

    Oppdatert kode i Javascript

--- menu.js
+++ menu.js
@@ -1,15 +1,24 @@
 let menuHTML = ""
-let selectedCategory = "HTML"
+let selectedCategory = []
 
 
 //Fått tak i categoriene
+
 resources.map((mappe, index) => menuHTML +=
-`<li id="${mappe.category}" class="item">${mappe.category}</li>` 
+`<li id="${mappe.category}" class="item"><button onclick="myFunction('${mappe.category}')">${mappe.category}</button></li>` 
+
 )
 
-console.log(menuHTML)
+
 
 document.getElementById("liste").innerHTML = menuHTML
-document.getElementById("HTML").addEventListener("click", myFunction);
+
+//document.getElementById("HTML").addEventListener("click", myFunction);
+
+function myFunction(navn) {
+   selectedCategory = resources.filter((info) => info.category == navn) 
+    console.log(selectedCategory)
+
+}
 
 
