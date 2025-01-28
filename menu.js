574bf0b30aa1ae14f822a7a1ac9db860cff80e03
Madelenkr <142605372+Madelenkr@users.noreply.github.com>
2025-01-28 19:38:19 +0100

    Koden er ferdig
    Kodingen er ferdig, mangler css på linkene mine

--- menu.js
+++ menu.js
@@ -1,8 +1,10 @@
 let menuHTML = ""
+let infoHTML = ""
 let selectedCategory = []
+let selectedCategory1 = []
 
 
-//Fått tak i categoriene
+//Fått tak i kategoriene
 
 resources.map((mappe, index) => menuHTML +=
 `<li id="${mappe.category}" class="item"><button onclick="myFunction('${mappe.category}')">${mappe.category}</button></li>` 
@@ -13,12 +15,27 @@ resources.map((mappe, index) => menuHTML +=
 
 document.getElementById("liste").innerHTML = menuHTML
 
-//document.getElementById("HTML").addEventListener("click", myFunction);
+myFunction("HTML")
 
 function myFunction(navn) {
-   selectedCategory = resources.filter((info) => info.category == navn) 
+   selectedCategory1 = resources.filter((info) => info.category == navn)
+   selectedCategory = selectedCategory1[0]
+
     console.log(selectedCategory)
 
-}
+    infoHTML += `<article class="item">
+                    <h2 id="topptext">${selectedCategory.category}</h2>
+                    <span id ="text">${selectedCategory.text}</span>
+                    
+                    
+                    <ul class="linker">
+                        ${selectedCategory.sources.map(source =>
+                        `<li><a href ="${source.url}">${source.title} </a> </li>`)}
+                    </ul>
+                </article>`
 
+                document.getElementById("liste").innerHTML = menuHTML + infoHTML
 
+                infoHTML = ""
+
+    }
